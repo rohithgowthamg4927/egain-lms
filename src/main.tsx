6fd1774b002a57133849fe30cbbb09351a775acb
rohithgowthamg4927 <rohithgowthamg4927@gmail.com>
2025-04-03 06:42:43 +0000

    Reverted to edit a7bd2e2d-4cc4-4090-be73-f0201f7b2326: "design"

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -6,8 +5,14 @@ import App from './App.tsx'
 import { AuthProvider } from './hooks/use-auth.tsx'
 import './index.css'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </BrowserRouter>
 );

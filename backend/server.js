4b60e97763b9f21802a20f38e7939747b66be8a4
rohithgowthamg4927 <rohithgowthamg4927@gmail.com>
2025-04-12 18:47:45 +0530

    remove rocket

--- backend/server.js
+++ backend/server.js
@@ -73,5 +73,5 @@ app.use((err, req, res, next) => {
 
 // Start server
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
